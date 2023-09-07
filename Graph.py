b9ec44284b0a6a0a0544315eeedcb7c985598657
lurdesss <3168761941503@ingenieria.usac.edu.gt>
2023-09-06 21:54:00 -0600

    Se carga archivo xml y se procesa

--- Graph.py
+++ Graph.py
@@ -6,14 +6,23 @@ class Graph():
         self.colorNodo = colorNodo
         self.dot = graphviz.Digraph('structs', filename=f'{self.nombreArchivo}.gv', node_attr={'shape':'egg' ,'color': f'{self.colorNodo}', 'style': 'filled', 'fontname': 'Helvetica'})
         
-    def agregar(self, nodoInicial, nodoSiguiente):
+    def agregar(self, nodoInicial, nodoSiguiente, direccion):
         if(nodoSiguiente != None):
             self.dot.node(str(nodoInicial.getId()), str(nodoInicial.getDato()))
             self.dot.node(str(nodoSiguiente.getId()), str(nodoSiguiente.getDato()))
             self.dot.edge(str(nodoInicial.getId()), str(nodoSiguiente.getId()))
+            self.dot.attr(rankdir=direccion)
 
-    def agregarElementosAnidados(self):
-        pass
+    def agregarElementosAnidados(self, nombreSenal, tiempo, amplitud, dimensionAmplitudes, direccion="TB"):
+        if(dimensionAmplitudes != None):
+            self.dot.node(nombreSenal,str(nombreSenal.getNombre()))
+            self.dot.node(tiempo,str(tiempo.getTiempo()))
+            self.dot.node(amplitud, str(amplitud.getAmplitud()))
+            self.dot.node(dimensionAmplitudes, str(dimensionAmplitudes.getDimensionAmplitudes()))
+            self.dot.edge(nombreSenal,tiempo)
+            self.dot.edge(nombreSenal,amplitud)
+            self.dot.edge(nombreSenal,dimensionAmplitudes)
+            self.dot.attr(rankdir=direccion)
     
     def generar(self):
         self.dot.render(outfile=f'files/{self.nombreArchivo}.png').replace('\\', '/')
--- Tiempo.py
+++ Tiempo.py
@@ -1,33 +1,13 @@
-from ListaSimple import ListaSimple
-from Amplitud import Amplitud
+#from ListaSimple import ListaSimple
+#from Amplitud import Amplitud
 
 class Tiempo():
     def __init__(self, tiempo, amplitud):
         self.tiempo = tiempo
         self.amplitud = amplitud
-        self.listaAmplitudes = ListaSimple()
-        self.llenarListaAmplitudes()
-        self.procesarPatron()
     
     def getTiempo(self):
         return self.tiempo
     
     def getAmplitud(self):
         return self.amplitud
-    
-    def llenarListaAmplitudes(self):
-        for i in range(1, int(self.amplitud)+1):
-            temporalAmplitud = Amplitud(i)
-            #lista de amplitudes
-            self.listaAmplitudes.agregarFinal(temporalAmplitud)
-        return self.listaAmplitudes
-
-    def imprimir(self): #prueba    -------------------------------------------------
-        print("_____Amplitudes para tiempo:", self.getTiempo() ,"_____")
-        objAmplitud = self.listaAmplitudes.getInicial()
-        while objAmplitud != None:
-            print(objAmplitud.getDato().getAmplitud())
-            objAmplitud = objAmplitud.getSiguiente()
-
-    def procesarPatron(self):
-        pass
